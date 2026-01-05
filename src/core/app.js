6c3c4c8ebbe122d484363ad4d691b2c18567aaa2
PortalEstiba <gamesarplay@gmail.com>
2026-01-05 08:42:38 +0100

    Actualizar app.js

--- src/core/app.js
+++ src/core/app.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const oraculoPage = document.getElementById('page-oraculo');
   const dashboardPage = document.getElementById('page-dashboard');
   const sueldometroPage = document.getElementById('page-sueldometro');
-
+  
+  oraculo?.render?.(oraculoPage);
   dashboard?.render?.(dashboardPage);
   sueldometro?.render?.(sueldometroPage);
-  oraculo?.render?.(oraculoPage);
 });
