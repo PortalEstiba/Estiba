e4559bbf09bbdf557a456e8d92569f6c38b0d434
PortalEstiba <gamesarplay@gmail.com>
2026-01-05 08:28:49 +0100

    Actualizar app.js

--- src/core/app.js
+++ src/core/app.js
@@ -1,11 +1,14 @@
 // Arranque seguro de la app
+import oraculo from '../modules/oraculo.js';
 import dashboard from '../modules/dashboard.js';
 import sueldometro from '../modules/sueldometro.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const oraculoPage = document.getElementById('page-oraculo');
   const dashboardPage = document.getElementById('page-dashboard');
   const sueldometroPage = document.getElementById('page-sueldometro');
 
   dashboard?.render?.(dashboardPage);
   sueldometro?.render?.(sueldometroPage);
+  oraculo?.render?.(oraculoPage);
 });
